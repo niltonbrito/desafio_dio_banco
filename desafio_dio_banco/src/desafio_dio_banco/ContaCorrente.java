faa6b91acf8c4d6c6af7c3e996b76f1ca78429ca
Nilton-Brito <nilton.brito@outlook.com>
2024-06-13 17:40:46 -0300

    Segundo Commit

--- desafio_dio_banco/src/desafio_dio_banco/ContaCorrente.java
+++ desafio_dio_banco/src/desafio_dio_banco/ContaCorrente.java
@@ -4,6 +4,9 @@ public class ContaCorrente extends Conta {
 
 	@Override
 	public void imprimirExtrato() {
-		System.out.println("Extrato da conta corrente");
+		System.out.println("=== Extrato da conta corrente ===");
+		System.out.println(String.format("Agencia: %d", super.agencia));
+		System.out.println(String.format("Numero da Conta: %d", super.numeroConta));
+		System.out.println(String.format("Saldo: R$ %.2f",super.saldo));
 	}
 }
--- desafio_dio_banco/src/desafio_dio_banco/ContaPoupanca.java
+++ desafio_dio_banco/src/desafio_dio_banco/ContaPoupanca.java
@@ -4,7 +4,10 @@ public class ContaPoupanca extends Conta {
 
 	@Override
 	public void imprimirExtrato() {
-		System.out.println("Extrato da conta poupança");
+		System.out.println("=== Extrato da conta poupança ===");
+		System.out.println(String.format("Agencia: %d", super.agencia));
+		System.out.println(String.format("Numero da Conta: %d", super.numeroConta));
+		System.out.println(String.format("Saldo: R$ %.2f",super.saldo));
 	}
 
 }
